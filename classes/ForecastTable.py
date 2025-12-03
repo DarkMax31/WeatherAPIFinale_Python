4f3acfb4415d9e61bc54893b6fcc999d09327709
DarkMax31 <maxime.carpentier63@gmail.com>
2025-12-03 08:28:21 +0100

    removeing not use fonction (not supported)

--- classes/ForecastTable.py
+++ classes/ForecastTable.py
@@ -9,7 +9,6 @@ class ForecastTable:    # Classe for displaying forecast data in a table
     def create_table(self):   # Create the table structure with headers and title
         self.table.title = "Prévisions météorologiques"
         self.table.field_names = ["Date", "Pluie (mm)", "Neige (mm)", "Transitions majeures"]
-        self.table.footer = ["Résumé des prévisions"]
 
     def add_data(self):  # Add forecast data rows to the table
         for forecast in self.forecast_details:
